f80cbad76dfab1ce15fb14bf5d73c44b8e13c88c
EstebanAN <116687559+EstebanAN@users.noreply.github.com>
2023-08-22 10:21:19 -0500

    Frist commit
    Prototipo sujeto a cambios

---
+++ FrontEnd/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ECU-911_Control_de_asistencia';
+}
