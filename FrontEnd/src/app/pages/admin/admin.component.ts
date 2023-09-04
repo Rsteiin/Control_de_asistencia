8a2a0f7cc47751404bf3406dd48c602508af19c6
EstebanAN <esteban.andaluz@nodeanalytics.net>
2023-09-04 17:56:04 -0500

    Update dashboard
    HU dashboard

--- FrontEnd/src/app/pages/admin/admin.component.ts
+++ FrontEnd/src/app/pages/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { meses, turnos } from '@app/shared/constantes/constantes';
 
 @Component({
   selector: 'app-admin',
@@ -9,11 +11,36 @@ export class AdminComponent implements OnInit {
   isLoading: boolean = false;
   instituciones = [{viewValue:"Todas", value:"-1"}];
   container = "id"
+  //select options 
+  years = [];
+  meses = meses;
+
+  //selected options 
+  selected_year = 0;
+  selected_month = "";
+
+  //turnos
+  turnos = turnos;
+  selected_turno = turnos[0].value;
 
   constructor() { }
 
   ngOnInit(): void {
-
+    //for years
+    let new_years = getYears();
+    this.years = new_years.years;
+    this.selected_year = new_years.actual_year;
+    //for months
+    this.selected_month = moment().format('MM');
+    
   }
+}
 
+const getYears = () => {
+  let actual_year = parseInt(moment().format('YYYY'));
+  let years = [];
+  for (let i = actual_year; i >= 2021; i--) {
+    years.push({value:i, viewValue:i});
+  }
+  return {years, actual_year};
 }
---
+++ FrontEnd/src/app/shared/constantes/constantes.ts
@@ -0,0 +1,21 @@
+export const meses = [
+  {value:"01", viewValue:"Enero"},
+  {value:"02", viewValue:"Febrero"},
+  {value:"03", viewValue:"Marzo"},
+  {value:"04", viewValue:"Abril"},
+  {value:"05", viewValue:"Mayo"},
+  {value:"06", viewValue:"Junio"},
+  {value:"07", viewValue:"Julio"},
+  {value:"08", viewValue:"Agosto"},
+  {value:"09", viewValue:"Septiembre"},
+  {value:"10", viewValue:"Octubre"},
+  {value:"11", viewValue:"Noviembre"},
+  {value:"12", viewValue:"Diciembre"}
+];
+
+export const turnos = [
+  {value:"Todos", viewValue:"Todos"},
+  {value:"Mañana", viewValue:"Mañana"},
+  {value:"Tarde", viewValue:"Tarde"},
+  {value:"Velada", viewValue:"Velada"},
+];
